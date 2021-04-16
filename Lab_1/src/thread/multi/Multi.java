d4f56a875f7203ed4c3827561f53da8f1ae55d5e
olena-pavlus <62425487+olena-pavlus@users.noreply.github.com>
2021-04-16 14:48:25 +0300

    Update Multi.java

--- Lab_1/src/thread/multi/Multi.java
+++ Lab_1/src/thread/multi/Multi.java
@@ -2,20 +2,13 @@ package thread.multi;
 
 import file.FileReader;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Set;
-
 public class Multi {
     public static void main(String[] args) throws InterruptedException {
 
         String str = FileReader.read();
         int numberOfThreads = 4;
         String findStr = "hello";
-        long time = System.currentTimeMillis();
         findElement(numberOfThreads, findStr, str);
-        System.out.println("\nTime - " + (double) (System.currentTimeMillis() - time));
     }
 
     private static void findElement(int numberOfThreads, String findStr, String str) throws InterruptedException {
@@ -24,23 +17,27 @@ public class Multi {
         int sizeOfFindStr = findStr.length();
         int range = sizeOfStr/numberOfThreads;
         String substr;
-        Thread searcher = null;
-
+        //Thread searcher = null;
+        Thread[] searcher = new Thread[numberOfThreads];
+        long time = System.currentTimeMillis();
         for (int i = 0; i <= numberOfThreads-1; i++) {
-            
+
             if (i == numberOfThreads-1) {
                 substr = str.substring(i*range, sizeOfStr);
-                searcher = new Thread(new Searcher(findStr, substr, i, range));
+                searcher[i] = new Thread(new Searcher(findStr, substr, i, range));
 
             } else {
                 substr = str.substring(i*range, i*range+range-1 + (sizeOfFindStr - 1));
-                searcher = new Thread(new Searcher(findStr, substr, i, range));
+                searcher[i] = new Thread(new Searcher(findStr, substr, i, range));
 
             }
-            searcher.start();
+            searcher[i].start();
+            //searcher[i].join();
         }
-        assert searcher != null;
-        searcher.join();
-
+        long time2 = System.currentTimeMillis() - time;
+        for (int i = 0; i <= numberOfThreads-1; i++) {
+            searcher[i].join();
+        }
+        System.out.println("\nTime - " + (double) time2);
     }
 }
